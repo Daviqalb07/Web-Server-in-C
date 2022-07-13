870b4cd10a72eb60bc8550ad3d1fbb88de23b58d
Marcos Lima <marcospl@alu.ufc.br>
2022-07-13 16:04:45 -0300

    bind e listen do server

--- server.c
+++ server.c
@@ -15,15 +15,22 @@ typedef struct {
     struct sockaddr_in address;
 } Host;
 
-
+void serverBind(Host *server){
+    if(bind(server->sockfd, (struct sockaddr*)&server->address, sizeof(server->address)) < 0){
+        perror("bind");
+        exit(1);
+    }
+    else 
+        printf("Ta de pe\n");
+}
 
 Host createServer(int port, char *address){
     Host host;
     host.sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if(host.sockfd < 0){
-        printf("Não deu certo.\n");
-        exit(0);
+        perror("socket");
+        exit(1);
     }
     else
         printf("Socket criado\n");
@@ -34,15 +41,30 @@ Host createServer(int port, char *address){
     host.address.sin_port = htons(port);
     host.address.sin_addr.s_addr = inet_addr(address);
 
+    serverBind(&host);
+
+    if(listen(host.sockfd, 2048) < 0){
+        perror("listen");
+        exit(1);
+    }
+    else
+    {
+        printf("Escutando\n");
+    }
+
     return host;
 }
 
+
+ 
 
 
 int main(int argc, char const *argv[])
 {
     Host server = createServer(PORT, IP_ADDR);
+
+
+    
+    Host clients[N_MAX_CLIENTS];
     return 0;
-
-
 }
